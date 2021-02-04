8e6de1c6544ea5e8062f22be729e588d5393adf2
özgür demir <emirozgur@yahoo.com>
2021-02-04 16:34:33 +0300

    recap_restore

---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear="2018",DailyPrice=150000,Description="Yeni gibi"},
+                new Car{CarId=2,BrandId=2,ColorId=2,ModelYear="2020",DailyPrice=150000,Description="Kusursuz"}
+            };
+
+        }
+
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAllByBrand(int cbrandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAllByColor(int colorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
