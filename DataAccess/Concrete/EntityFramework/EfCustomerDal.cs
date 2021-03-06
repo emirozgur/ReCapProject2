20851f08f59c709d333666e16c57536b622a8a8f
özgür demir <emirozgur@yahoo.com>
2021-03-06 22:09:21 +0300

    11.Gün  ödev

--- DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    class EfCustomerDal : EfEntityRepositoryBase<Customer, GalleryContext>, ICustomerDal
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer, GalleryContext>, ICustomerDal
     {
         // Burada customer tablosu ile diğer tabloların join kodları olacak
         public List<CustomerDetailDto> GetCarDetails()
